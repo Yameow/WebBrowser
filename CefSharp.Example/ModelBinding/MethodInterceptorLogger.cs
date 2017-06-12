58dc14e9a5bdf4d16799aa5ce9bef8faa66ac32d
nnuba <nnuba@LAPTOP-IJ3V2CCB>
2017-06-12 23:29:34 +0800

    webbrowser demo init

---
+++ CefSharp.Example/ModelBinding/MethodInterceptorLogger.cs
@@ -0,0 +1,21 @@
+﻿// Copyright © 2010-2017 The CefSharp Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+using System;
+using System.Diagnostics;
+using CefSharp.ModelBinding;
+using CefSharp;
+
+namespace CefSharp.Example.ModelBinding 
+{
+    public class MethodInterceptorLogger : IMethodInterceptor
+    {
+        object IMethodInterceptor.Intercept(Func<object> method, string methodName)
+        {
+            object result = method();
+            Debug.WriteLine("Called " + methodName);
+            return result;
+        }
+    }
+}
--- WebBrowser/Program.cs
+++ WebBrowser/Program.cs
@@ -1,22 +1,86 @@
-﻿using System;
+﻿using CefSharp;
+using CefSharp.Example;
+using CefSharp.Example.Handlers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WebBrowser
+namespace zyj.WebBrowser
 {
-    static class Program
+    public class Program
     {
-        /// <summary>
-        /// 应用程序的主入口点。
-        /// </summary>
         [STAThread]
-        static void Main()
+        public static int Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            const bool simpleSubProcess = false;
+
+            Cef.EnableHighDPISupport();
+
+            //NOTE: Using a simple sub processes uses your existing application executable to spawn instances of the sub process.
+            //Features like JSB, EvaluateScriptAsync, custom schemes require the CefSharp.BrowserSubprocess to function
+            //            if (simpleSubProcess)
+            //            {
+            //                var exitCode = Cef.ExecuteProcess();
+
+            //                if (exitCode >= 0)
+            //                {
+            //                    return exitCode;
+            //                }
+
+            //#if DEBUG
+            //                if (!System.Diagnostics.Debugger.IsAttached)
+            //                {
+            //                    MessageBox.Show("When running this Example outside of Visual Studio" +
+            //                                    "please make sure you compile in `Release` mode.", "Warning");
+            //                }
+            //#endif
+
+            //                var settings = new CefSettings();
+            //                settings.BrowserSubprocessPath = "CefSharp.WinForms.Example.exe";
+
+            //                Cef.Initialize(settings);
+
+            //                var browser = new SimpleBrowserForm();
+            //                Application.Run(browser);
+            //            }
+            //            else
+            //            {
+#if DEBUG
+            if (!System.Diagnostics.Debugger.IsAttached)
+            {
+                MessageBox.Show("When running this Example outside of Visual Studio" +
+                                "please make sure you compile in `Release` mode.", "Warning");
+            }
+#endif
+
+            const bool multiThreadedMessageLoop = true;
+
+            var browser = new WebBrowser(multiThreadedMessageLoop);
+            //var browser = new SimpleBrowserForm();
+            //var browser = new TabulationDemoForm();
+
+            IBrowserProcessHandler browserProcessHandler;
+
+            if (multiThreadedMessageLoop)
+            {
+                browserProcessHandler = new BrowserProcessHandler();
+            }
+            else
+            {
+                //Get the current taskScheduler (must be called after the form is created)
+                var scheduler = TaskScheduler.FromCurrentSynchronizationContext();
+
+                browserProcessHandler = new WinFormsBrowserProcessHandler(scheduler);
+            }
+
+            CefExample.Init(osr: false, multiThreadedMessageLoop: multiThreadedMessageLoop, browserProcessHandler: browserProcessHandler);
+
+            Application.Run(browser);
+            return 0;
         }
+
     }
+    //}
 }
